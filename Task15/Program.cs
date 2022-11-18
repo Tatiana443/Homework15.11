478f2f2771ab7e605cfc3fa87c699492cbfbd5a6
Tatiana443 <tvb399@yandex.ru>
2022-11-18 17:06:29 +0600

    Project Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите цифру дня недели: ");
+int daynum = Convert.ToInt32(Console.ReadLine());
+void CheckDay (int daynum) 
+{
+  if (daynum == 6 || daynum == 7) 
+  {
+  Console.WriteLine("Этот день выходной");
+  }
+  else if (daynum < 1 || daynum > 7) 
+  {
+    Console.WriteLine("введены неправильные данные");
+  }
+  else Console.WriteLine("Этот день рабочий");
+}
+CheckDay (daynum);
+
+
+
+
+
