435ac2ff8845548252f33cadfdd854f05fe244ca
Tatiana443 <tvb399@yandex.ru>
2022-11-18 09:49:53 +0600

    Project Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число num:");
+int num = Convert.ToInt32(Console.ReadLine());
+int secondDigit = (num / 10) % 10;
+if (num < 100 | num > 999) Console.WriteLine("Число не является трехзначным");
+else Console.WriteLine(secondDigit);
