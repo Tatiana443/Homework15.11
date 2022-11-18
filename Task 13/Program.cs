18048811f888e2fbffec09cb8f108bb14c0ac988
Tatiana443 <tvb399@yandex.ru>
2022-11-18 10:34:11 +0600

    Project Task13

---
+++ Task 13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int ThirdDigit(int num)
+{
+    while (num >= 1000)
+    {
+        num = num / 10;
+    }
+    return num % 10;
+}
+if (num < 100) Console.WriteLine("Третьей цифры в числе нет");
+else Console.WriteLine(ThirdDigit(num));
